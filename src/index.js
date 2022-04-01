8ba80d00bf39431c431a30cb361dfbfedbd23469
Anton Balter <antbalter@gmail.com>
2022-04-02 01:53:30 +0300

    hw#32 class solution

--- src/index.js
+++ src/index.js
@@ -1,51 +1,38 @@
-/*****************************************HW #32 ***********/
+/*****************************************HW #32 */
 function intersection(set1, set2) {
-    //TODO write function returning array of common numbers between two sets
+    //function returning array of common numbers between two sets
     //that is the numbers existing in both sets
-    const ar1 = Array.from(set1);
-    const ar2 = Array.from(set2);
-    const ar3 = [];
-    ar1.forEach(n => {
-        if (ar2.indexOf(n) !== -1) {
-            ar3.push(n);
-        }
-    });
-    return ar3;
+    return Array.from(set1).filter(Set.prototype.has.bind(set2));
 }
-const set1 = new Set([1, 33, 46, 1, 1326, 22, 1, 45, 1]);
-const set2 = new Set([27, 5, 33, 465, 21, 1, 78, 8, 1, 22]);
-const resAr = intersection(set1, set2);
-console.log(resAr);
 function sbtract(set1, set2) {
-    //TODO write function returning array of numbers from set1 that don't exist in the 
+    //write function returning array of numbers from set1 that don't exist in the 
     //set2
-    const ar1 = Array.from(set1);
-    const ar2 = Array.from(set2);
-    const ar3 = [];
-    ar1.forEach(n => {
-        if (ar2.indexOf(n) === -1) {
-            ar3.push(n);
-        }
-    });
-    return ar3;
+    return Array.from(set1).filter(n => !set2.has(n));
 }
-const resAr1 = sbtract(set1, set2);
-console.log(resAr1);
 function getSortedOccurrences(array) {
-    //TODO
-    //write function returning array of occurrences 
+    // function returning array of occurrences 
     //each occurrency contains a string from the given array and how many times it occures in the array
     // a result array should be sorted in the descending order of the occurrences and ascending order of the strings
     //example: the given array is ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn']
     //result: [{str: 'lmn', count: 3}, {str: 'cd', count: 2}, {str: 'a', count:1}, {str: 'ab', count:1}]
     //implementation notes: to use Map<string, number>
-    const occurencyMap = new Map();
-    array.forEach(n => occurencyMap.has(n) ? occurencyMap.set(n, occurencyMap.get(n) + 1) : occurencyMap.set(n, 1));
-    const occurrencyArr = [];
-    occurencyMap.forEach((value, key) => occurrencyArr.push({ str: key, count: value }));
-    occurrencyArr.sort((a, b) => a.count - b.count ? b.count - a.count : a.str.localeCompare(b.str));
-    return occurrencyArr;
+    const mapOccurrences = array.reduce((map, str) => {
+        var _a;
+        const count = (_a = map.get(str)) !== null && _a !== void 0 ? _a : 0;
+        map.set(str, count + 1);
+        return map;
+    }, new Map());
+    return Array.from(mapOccurrences)
+        .map(e => ({ str: e[0], count: e[1] }))
+        .sort((o1, o2) => o2.count - o1.count || o1.str.localeCompare(o2.str));
 }
-const strAr = ['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn'];
-const foo = getSortedOccurrences(strAr);
-console.log(foo);
+/**************************************************************************** */
+Set.prototype.toString = function () { return Array.from(this).toString(); };
+const set1 = new Set([1, 2, 3, 4, 5]);
+const set2 = new Set([2, 5, 7, -10]);
+console.log(`set1: ${set1}`);
+console.log(`set2: ${set2}`);
+console.log(`intersection(set1, set2)=${intersection(set1, set2)}`);
+console.log(`sbtract(set1, set2) = ${sbtract(set1, set2)}`);
+console.log('__'.repeat(20));
+console.log(getSortedOccurrences(['lmn', 'ab', 'a', 'cd', 'lmn', 'cd', 'lmn']));
